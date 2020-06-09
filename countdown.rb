ea6e21ba7b3004c9cbe14d63fb3f8dfa61f6a2e7
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-09 04:39:23 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
-
-def countdown
+ 
+def countdown(seconds_left)
+  until seconds_left == 0 
+  puts "#{seconds_left} SECOND(S)!"
+  seconds_left -= 1
 end
+"HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep(seconds_left)
+until seconds_left < 1 
+puts "#{seconds_left} SECOND(S)"
+seconds_left -= 1 
+sleep 5
+end
+ "HAPPY NEW YEAR!"
+ end
+ 
