9bc54d6548cb6a4318c54b9951ad93eb0016de9a
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-09 04:32:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
-
+ 
 def countdown(seconds_left)
   until seconds_left == 0 
   puts "#{seconds_left} SECOND(S)!"
   seconds_left -= 1
 end
-end
-
+"HAPPY NEW YEAR!"
+end 
  
