4336801a6520eb240e388bde459199cc348b4b17
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-09 04:09:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(seconds_left)
+  until seconds_left < 1 
+  puts "#{seconds_left} SECOND(S)!"
+  seconds_left -= 1 
 end
+ "HAPPY NEW YEAR!"
+ end
+ 
+ # number -= 1 #=> number = number - 1 
+ 
+ def countdown_with_sleep(second _left)
+   until seconds_left < 1 
+   puts "#{seconds_left} SECOND(S)"
+   seconds_left -= 1 
+   sleep 5 
+ end
+  "HAPPY NEW YEAR!"
+ end
